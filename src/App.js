dbe53138e5fa186be5e51c536f9fe6d212be2c72
Anshul Raheja <anshul.r1610@gmail.com>
2021-07-21 18:18:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name='Desktop'>
+        <Folder name='Music' />
+        <Folder name='Pictures'>
+          <File name='photo.jpg' />
+        </Folder>
+      </Folder>
+      <Folder name='Downloads'>
+        <Folder name='Setups' />
+        <File name='sheet.xls' />
+        <File name='song.mp4' />
+      </Folder>
     </div>
   );
 }
+
+const Folder = ({ name, children }) => {
+  const [isOpen, setisOpen] = useState(false);
+  const direction = isOpen ? "down" : "right";
+  const handleClick = () => {
+    setisOpen(!isOpen);
+  };
+
+  return (
+    <div style={{ margin: "1px 0px 0px 10px" }}>
+      <span onClick={handleClick}>
+        <i className='bi bi-folder-fill'></i>
+        <i class={`bi bi-chevron-${direction}`}></i>
+      </span>
+      {name}
+      <div style={{ marginLeft: "10px" }}>{isOpen ? children : null}</div>
+    </div>
+  );
+};
+
+const File = ({ name }) => {
+  const fileExtension = name.split(".")[1];
+  const fileIcon = {
+    mp4: "bi-file-earmark-music",
+    jpg: "bi-file-earmark-play-fill",
+    xls: "bi-file-earmark-spreadsheet-fill",
+  };
+  return (
+    <div
+      className={`bi ${fileIcon[fileExtension]}`}
+      style={{ marginLeft: "10px" }}
+    >
+      {name}
+    </div>
+  );
+};
 
 export default App;
